50543fdb1fdfd0d9668adde0ddc7ec7a08d3f0ce
Ayaghoul <eyalghoul@gmail.com>
2025-07-28 18:52:57 +0100

    first commit

---
+++ user-api/config/connectDB.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect(process.env.URI);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = ConnectDB;
+require("dotenv").config({ path: "./config/.env" });
